4ed88567bb1b4c7a1aef24ce32aefd87ab96c9c4
Patrick Low <lowyuenan@gmail.com>
2020-03-29 23:23:24 +0800

    learn threejs

---
+++ threejs/precache-manifest.57c8390a50ce770311a5730f0e32ca0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004b8b66904ee313c54f74d835619972",
+    "url": "/threejs/index.html"
+  },
+  {
+    "revision": "1da1e744ad6e8f61ebee",
+    "url": "/threejs/static/css/main.765bdf1c.chunk.css"
+  },
+  {
+    "revision": "586f2fe561024c0acab1",
+    "url": "/threejs/static/js/2.1f692d6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/threejs/static/js/2.1f692d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da1e744ad6e8f61ebee",
+    "url": "/threejs/static/js/main.f756b007.chunk.js"
+  },
+  {
+    "revision": "933b30ae2bb5f4b6c451",
+    "url": "/threejs/static/js/runtime-main.8a0685c1.js"
+  }
+]);
